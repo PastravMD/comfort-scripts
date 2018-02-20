8a4459c23a384b5def690d801fcf103d41fe8d65
Mihai Dragan <mihai_dragan@outlook.com>
2018-02-20 00:15:21 +0000

    added sanitized mutt config

--- general-setup.sh
+++ general-setup.sh
@@ -3,9 +3,10 @@
 sudo mv /etc/bash.bashrc ./old-bash.bashrc
 sudo cp ./bash.bashrc /etc/bash.bashrc
 
-sudo cp dotXdefaults ~/.Xdefaults
+cp ./dotXdefaults ~/.Xdefaults
+cp ./muttrc ~/.mutt/muttrc
 
 echo "setxkbmap -option caps:swapescape" >> ~/.profile
 
 # apps to get:
-# ranger rxvt-unicode-256color git gcc wget 
+# ranger rxvt-unicode-256color mutt git gcc wget 
